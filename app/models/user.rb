6837b006ffea06d352fc062ae597400e7aab1a79
rahadi rafsanjani <rahadirafsanjani@gmail.com>
2024-10-08 13:58:36 +0700

    feat: adding validation for user and relation between author and book

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, format: /\A[^0-9`!@#\$%\^&*+_=]+\z/
+  validates :password, presence: true
 end
